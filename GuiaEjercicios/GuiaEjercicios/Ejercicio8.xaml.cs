5e2ebd6b01905fb0a646f42bd4be9ca135e62b89
samuelreyesiglesias <samuelreyesiglesias@gmail.com>
2019-07-02 21:57:58 -0600

    Nuevos cambios ejercicio 8

---
+++ GuiaEjercicios/GuiaEjercicios/Ejercicio8.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GuiaEjercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ejercicio8 : ContentPage
+    {
+        public Ejercicio8()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string diaSeleccionado;
+            switch (EntryDiaSemana.Text)
+            {
+                case "1":
+                    diaSeleccionado = "Lunes";
+                    break;
+                case "2":
+                    diaSeleccionado = "Martes";
+                    break;
+                case "3":
+                    diaSeleccionado = "Miercoles";
+                    break;
+                case "4":
+                    diaSeleccionado = "Jueves";
+                    break;
+                case "5":
+                    diaSeleccionado = "Viernes";
+                    break;
+                case "6":
+                    diaSeleccionado = "Sabado";
+                    break;
+                case "7":
+                    diaSeleccionado = "Domingo";
+                    break;
+            }
+            DisplayAlert("Dia seleccionado:", diaSeleccionado, "ok");
+        }
+    }
+}
